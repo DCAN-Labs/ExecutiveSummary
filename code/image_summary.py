4ef4daab1dbe7587547e9194796f2583fb3fc97c
unknown <bucklesh@MSBB493.ohsum01.ohsu.edu>
2015-12-30 17:33:34 -0800

    adjust images_dict to what we actually have for each sub. add image examples for testing.

--- code/image_summary.py
+++ code/image_summary.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 """
 __author__ = 'Shannon Buckley', 12/27/15
 """
@@ -9,19 +9,27 @@ from os import path
 import logging
 
 
-_log = logging.getLogger('my_log.log')
-
-_log.info('Starting Up...')
-
-images_dict = {'temp_1' : 'T1-Axial-InferiorTemporal-Cerebellum',
-               'temp_2' : 'T2-Axial-InferiorTemporal-Cerebellum',
-               'temp_13': 'T1-Coronal-Hippocampus',
-               'temp_14': 'T2-Coronal-Hippocampus',
-               'temp_15': 'T1-Sagittal-CorpusCallosum',
-               'temp_16': 'T2-Coronal-CorpusCallosum',
-               }
-
-
+# describes existing set of structural images... may need adjustments to other locations / names
+images_dict = {
+    'temp_1': 'T1-Axial-InferiorTemporal-Cerebellum',
+    'temp_2': 'T2-Axial-InferiorTemporal-Cerebellum',
+    'temp_3': 'T1-Axial-BasalGangila-Putamen',
+    'temp_4': 'T2-Axial-BasalGangila-Putamen',
+    'temp_5': 'T1-Axial-SuperiorFrontal',
+    'temp_6': 'T2-Axial-SuperiorFrontal',
+    'temp_7': 'T1-Coronal-PosteriorParietal-Lingual',
+    'temp_8': 'T2-Coronal-PosteriorParietal-Lingual',
+    'temp_9': 'T1-Coronal-Caudate-Amygdala',
+    'temp_10': 'T2-Coronal-Caudate-Amygdala',
+    'temp_11': 'T1-Coronal-OrbitoFrontal',
+    'temp_12': 'T2-Coronal-OrbitoFrontal',
+    'temp_13': 'T1-Sagittal-Insula-FrontoTemporal',
+    'temp_14': 'T2-Sagittal-Insula-FrontoTemporal',
+    'temp_15': 'T1-Sagittal-CorpusCallosum',
+    'temp_16': 'T2-Sagittal-CorpusCallosum',
+    'temp_17': 'T1-Sagittal-Insula-Temporal-HippocampalSulcus',
+    'temp_18': 'T2-Sagittal-Insula-Temporal-HippocampalSulcus'
+    }
 
 
 def rename_image(img_path):
@@ -37,8 +45,9 @@ def rename_image(img_path):
         os.rename(img_path, new_file_path)
 
 
-def get_png_count(path):
+def get_epi_count(path):
 
+    pass
     
 
 def rename_structural_images(path):
@@ -48,8 +57,11 @@ def rename_structural_images(path):
         quit()
 
 
-
 def main():
+
+    _log = logging.getLogger('my_log.log')
+
+    _log.info('Starting Up...')
 
     parser = argparse.ArgumentParser(description='Maker of Image Summaries.')
 
@@ -67,5 +79,5 @@ def main():
 
 if __name__ == '__main__':
 
-    rename_image()
+    main()
 
