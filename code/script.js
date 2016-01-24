2ec453abbca35cbdcb8b61f5c9724452c57ff084
shannon buckley <buckley.shannont@gmail.com>
2016-01-24 10:38:56 -0800

    add Next button to template and adjust epi-value for testing. add button style. simplify script.js by 1 line, sure.

--- code/script.js
+++ code/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-    var t1_tr = $('#t1_tr');
 
-    if(t1_tr.text() != '2400.0') {
+    if($('#t1_tr').text() != '2400.0') {
         $('#t1_tr').addClass('out-of-range');
     };
+
+
+
 });
