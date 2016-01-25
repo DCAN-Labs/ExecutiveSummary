1c6d8d8ea123543d02410ffbe235fe21e240499e
shannon buckley <buckley.shannont@gmail.com>
2016-01-24 19:14:50 -0800

    small adjustments to styles, add draggable to most panels, add div wrappers around panels

--- code/script.js
+++ code/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
-
     if($('#t1_tr').text() != '2400.0') {
         $('#t1_tr').addClass('out-of-range');
     };
 
+    $('div.params').draggable();
 
+    $('div.grayords').resizable();
+
+    $('div.grayords').draggable();
+
+    $('div.epi').draggable();
 
 });
