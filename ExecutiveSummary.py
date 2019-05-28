b7bc8d5a322ec28c220e2451de22855c33a28714
Kathy Snider <sniderk@ohsu.edu>
2019-05-28 13:31:43 -0700

    Only need one level now.

--- ExecutiveSummary.py
+++ ExecutiveSummary.py
@@ -182,8 +182,10 @@ def init_summary(proc_files, summary_dir):
 
         # TODO: do this in the script!
         # Copy the placeholders to be used if we cannot find a file.
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-        program_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+        #script_dir = os.path.dirname(os.path.realpath(__file__))
+        #program_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+
+        program_dir = os.path.dirname(os.path.realpath(__file__))
         placeholder_dir = os.path.join(program_dir, 'placeholder_pictures')
         square = find_and_copy_file(placeholder_dir, 'square_placeholder_text.png', images_path)
         rectangle = find_and_copy_file(placeholder_dir, 'rectangular_placeholder_text.png', images_path)
